283c03369b9f7cd634dd5609b7b0ce90cf136f1c
AdalZanabria <adal.zanabria@gmail.com>
2022-02-06 03:04:25 -0600

    Código creado.

---
+++ PlatziModoCine.js
@@ -0,0 +1,40 @@
+(function platziModoCine() {
+    // Variables para modificar los elementos
+    const container = document.querySelector(".MaterialView");
+    const controlsContainer = document.querySelector(".vjs-control-bar");
+    const videoContainer = document.querySelector(".MaterialView-video-item");
+    // Variable para cambiar entre modos
+    let cine = false;
+
+    // Variables para capturar el estado por defecto, el Modo Cine "off"
+    const defaultColumnas = container.style.gridTemplateColumns;
+    const defaultAreas = container.style.gridTemplateAreas;
+    const defaultDisplay = container.style.display;
+    const defaultMaxWidth = videoContainer.style.maxWidth;
+
+    // Crear y poner el botón de Modo Cine
+    let botonCine = document.createElement("button");
+    botonCine.type = "button";
+    botonCine.innerText = "Modo Cine";
+    controlsContainer.appendChild(botonCine);
+    botonCine.style.cursor = "pointer";
+    botonCine.addEventListener("click", cambioModo);
+
+    function cambioModo() {
+        cine ? modoNormal() : modoCine();
+        cine = !cine;
+    }
+
+    function modoCine() {
+        container.style.gridTemplateColumns = "1fr";
+        container.style.gridTemplateAreas = '"video" "content" "community"';
+        container.style.display = "grid";
+        videoContainer.style.maxWidth = "100vw";
+    }
+    function modoNormal() {
+        container.style.gridTemplateColumns = defaultColumnas;
+        container.style.gridTemplateAreas = defaultAreas;
+        container.style.display = defaultDisplay;
+        videoContainer.style.maxWidth = defaultMaxWidth;
+    }
+})();
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Platzi-Modo-Cine
-Agrega un botón al reproductor para cambiar a modo cine estilo YouTube.
+# Platzi Modo Cine
+
+## Agrega un botón al reproductor para cambiar a modo cine estilo YouTube en pantallas grandes.
+
+El _modo cine_ hace que el reproductor cubra el 100% del ancho del viewport, escondiendo el panel de comunidad.
+
+En pantallas de tamaños aproximados al 1080p, cuando se quiere ver Platzi a mitad de pantalla (Como para tener por mitad el video de la clase y la otra mitad el editor de texto y seguir el curso) este cambia su estilo automáticamente usando responsive design, pero si se quiere ver de la misma forma en pantalla dividida cuando la pantalla es de mayor resolución (Ejemplo 4K), el layout se vuelve el mismo al de pantalla completa ~1080, haciendo que aparezca el panel de comunidad y le quite espacio al reproductor de video, lo que puede resultar en una dificultad de lectura del contenido mostrado en el video.
+
+## ¿Cómo lo puedo probar?
+
+Copia todo el código dentro del archivo _PlatziModoCine.js_ y pégalo en la consola de tu navegador, al presionar _Enter_ aparecerá un botón en la barra de controles del reproductor que dice _"Modo Cine"_ al presionarlo cambiará entre los dos modos.
+
+### Funciona solo en resoluciones que el responsive design no esconda por defecto el panel de comunidad.
