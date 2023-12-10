31e41f25c8babebc5fe8d4a1e552782275bb8560
Adal Zanabria <adal.zanabria@gmail.com>
2023-12-10 01:40:48 -0600

    Refactorización de lógica. TODO: Incrementar la altura del reproductor en modo cine. w:100vw h:auto

--- PlatziModoCine.js
+++ PlatziModoCine.js
@@ -21,50 +21,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
         if (videoContainer && videoElement && !botonCine) {
             console.log("Platzi Modo Cine: Si hay video.");
-
             // Crear y poner el botón de Modo Cine en el reproductor
-            botonCine = document.createElement("button");
-            botonCine.type = "button";
-            botonCine.innerText = "Modo Cine";
-            botonCine.style.margin = "0px 5px"
-            document.querySelector(".vjs-control-bar").appendChild(botonCine);
-            console.log("Botón creado.");
-
-            // Leer el tamaño del display y si es menor a 1280 platzi modo cine manda una alerta
-            if (window.innerWidth < 1280) {
-                botonCine.style.color = 'red';
-                botonCine.addEventListener("click", () => alert("Platzi Modo Cine solo funciona en ventanas con resoluciones mayores a 1280px de ancho. \nCambia la resolución de la ventana y recarga la página para poderla usar."));
-            } else {
-                platziModoCine(videoContainer, botonCine);
-            }
+            botonCine = crearBotonCine();
+            setupBotonCine(videoContainer, botonCine);
         } else if (!videoContainer || !videoElement) {
             console.log("Platzi Modo Cine: No hay video.");
-            botonCine = null;
+            removerBotonCine();
         }
     }
 
-    function platziModoCine(videoContainer, botonCine) {
-        console.log("Inicia función Platzi Modo Cine.");
-        // Variables para modificar los elementos
-        const childElements = Array.from(videoContainer.children);
+    function crearBotonCine() {
+        const botonCine = document.createElement("button");
+        botonCine.type = "button";
+        botonCine.innerText = "Modo Cine";
+        botonCine.style.margin = "0px 5px";
+        document.querySelector(".vjs-control-bar").appendChild(botonCine);
+        console.log("Botón creado.");
+        return botonCine;
+    }
 
-        // Bandera para saber si esta activado el modo cine
+    function setupBotonCine(videoContainer, botonCine) {
+        const childElements = Array.from(videoContainer.children);
         let cine = false;
 
-        // Variables de los estilos de los diferentes estados
-        const defaultColumnas = ['1 / 2', '1 / 2', '1 / 2', '2 / 3', 'auto'];
-        const columnasModoCine = ['1 / 2', '1 / 3', '1 / 2', '2 / 3', 'auto'];
-
-        const defaultDisplay = ['none', 'block', 'none', 'block', 'none'];
-        const displayModoCine = ['none', 'block', 'block', 'none', 'none'];
-
-        botonCine.style.cursor = "pointer";
-        botonCine.addEventListener("click", cambioModo);
+        // Leer el tamaño del display y si es menor a 1280, mostrar mensaje de alerta
+        if (window.innerWidth < 1280) {
+            disableBoton();
+        } else {
+            botonCine.style.cursor = "pointer";
+            botonCine.addEventListener("click", cambioModo);
+        }
 
         function cambioModo() {
-            cine ? toggleModo(defaultColumnas, defaultDisplay) : toggleModo(columnasModoCine, displayModoCine);
-            cine = !cine;
-            cine ? botonCine.style.color = '#9ccd49' : botonCine.style.color = 'inherit';
+                cine ? resetChanges() :
+                    toggleModo(['1 / 2', '1 / 3', '1 / 2', '2 / 3', 'auto'], ['none', 'block', 'block', 'none', 'none']);
+                cine = !cine;
+                cine ? botonCine.style.color = '#9ccd49' : botonCine.style.color = 'inherit';
         }
 
         function toggleModo(arrayColumnas, arrayDisplays) {
@@ -72,6 +64,31 @@ document.addEventListener('DOMContentLoaded', function () {
                 element.style.gridColumn = arrayColumnas[index];
                 element.style.display = arrayDisplays[index];
             });
+        }
+
+        function resetChanges() {
+            console.log("Estilos reiniciados");
+            childElements.forEach((element) => {
+                // Restaurar los estilos originales si existen
+                element.style.gridColumn = '';
+                element.style.display = '';
+                // Eliminar estilos inline si fueron añadidos
+                element.removeAttribute('style');
+            });
+        }
+
+        function disableBoton() {
+            console.log("Botón deshabilitado");
+            botonCine.style.color = 'red';
+            botonCine.addEventListener("click", () => alert("Platzi Modo Cine solo funciona en ventanas con resoluciones mayores a 1280px de ancho. \nCambia la resolución de la ventana y recarga la página para poderla usar."));
+        }
+    }
+
+    function removerBotonCine() {
+        if (botonCine) {
+            botonCine.removeEventListener("click", cambioModo);
+            botonCine.parentNode.removeChild(botonCine);
+            botonCine = null;
         }
     }
 });
